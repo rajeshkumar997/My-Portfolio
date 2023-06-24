9856369071b856321c3b007f115cb7fca34aaf37
Rajesh Kumar <rajekumar7589@gmail.com>
2023-06-24 16:29:53 +0530

    add resume

--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
-import {BsLinkedin} from 'react-icons/bs'
-import {BsGithub} from 'react-icons/bs'
-import {BsFacebook} from 'react-icons/bs'
+import { BsLinkedin } from 'react-icons/bs'
+import { BsGithub } from 'react-icons/bs'
+import { BsFacebook } from 'react-icons/bs'
 
 const HeaderSocials = () => {
   return (
     <div className='header__socials'>
-        <a href='https://www.linkedin.com/in/rajesh-kumar-999a8b1bb/' target="_blank"><BsLinkedin /></a>
-        <a href='https://github.com/rajeshkumar997' target="_blank"><BsGithub /></a>
-        <a href='https://www.facebook.com/RajeshVishwa978/' target="_blank"><BsFacebook /></a>
+      <a href='https://www.linkedin.com/in/rajeshkumar997/' target="_blank"><BsLinkedin /></a>
+      <a href='https://github.com/rajeshkumar997' target="_blank"><BsGithub /></a>
+      <a href='https://www.facebook.com/RajeshVishwa978/' target="_blank"><BsFacebook /></a>
     </div>
   )
 }
